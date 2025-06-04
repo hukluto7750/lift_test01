d460aecf06696bd900ff0a1caacd01c132949415
hukuto7750 <hukuto7750@gmail.com>
2025-06-04 14:10:13 +0800

    init

--- liff-starter.js
+++ liff-starter.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     const useNodeJS = false;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "1655539275-LnQZ82GO";   // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "2007523880-Xkb23mKp";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = "";
